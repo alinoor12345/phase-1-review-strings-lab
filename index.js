ec14f793f77614a171f5808468da9024dff03e5b
Ali Omar <ali.noor.student.moringaschool.com git config --gobal Ali abdullahigit config --global user.email ali.noor.student.moringaschool.com>
2022-11-29 15:38:50 +0300

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`; 
+// AssertionError: expected 'Welcome, Grace Hopper' to  contain 'Grace Hopper'
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+//const shortGreeting = "Welcome, "
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
